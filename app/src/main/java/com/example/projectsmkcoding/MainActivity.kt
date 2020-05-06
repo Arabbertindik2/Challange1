5b89b382d5f24ba9a5865ef46e56bb9a4f65e06a
Arabbertindik2 <58155980+arabbertindik2@users.noreply.github.com>
2020-05-06 16:54:05 +0700

    simpan

--- app/src/main/java/com/example/projectsmkcoding/MainActivity.kt
+++ app/src/main/java/com/example/projectsmkcoding/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.etumur
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class MainActivity : AppCompatActivity() {
     var edtname: EditText? = null
     var spinnerjks: Spinner? = null
     var edtemail: EditText? = null
     var edttelp: EditText? = null
+    var edtumur: EditText? = null
     var edtAlamat: EditText? = null
+
     private var nameinput: String = ""
     private var emailinput: String = ""
     private var telpinput: String = ""
+    private var umurinput: String = ""
     private var alamatinput: String = ""
     private var genderinput: String = ""
 
@@ -24,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         edtname = findViewById(R.id.etname)
         edtemail = findViewById(R.id.etemail)
+        edtumur = findViewById(R.id.etumur)
         spinnerjks = findViewById(R.id.spinnerjk)
         edttelp = findViewById(R.id.ettelp)
         edtAlamat = findViewById(R.id.etalamat)
@@ -36,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         nameinput = etname.text.toString()
         emailinput = etemail.text.toString()
         telpinput = ettelp.text.toString()
+        umurinput = etumur.text.toString()
         alamatinput = etalamat.text.toString()
         genderinput = spinnerjk.selectedItem.toString()
 
@@ -48,6 +55,7 @@ class MainActivity : AppCompatActivity() {
             ) -> tampilToast("Jenis Kelamin harus dipilih")
             emailinput.isEmpty() -> etemail.error = "Email tidak boleh kosong"
             telpinput.isEmpty() -> ettelp.error = "Telp tidak boleh kosong"
+            umurinput.isEmpty()-> etumur.error = "umur kosong"
             alamatinput.isEmpty() -> etalamat.error = "Alamat tidak boleh "
 
             else -> {
@@ -74,6 +82,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun gotoProfilAvtivity() {
         val Intent = Intent(this, ProfileActivity::class.java)
+
+        val bundle = Bundle()
+        bundle.putString("nama1", nameinput)
+        bundle.putString("gender", genderinput)
+        bundle.putString("umur1", umurinput)
+        bundle.putString("email", emailinput)
+        bundle.putString("telp1", telpinput)
+        bundle.putString("alamat1", alamatinput)
+
+        Intent.putExtras(bundle)
         startActivity(Intent)
     }
 }
--- app/src/main/java/com/example/projectsmkcoding/ProfileActivity.kt
+++ app/src/main/java/com/example/projectsmkcoding/ProfileActivity.kt
@@ -2,11 +2,32 @@ package com.example.projectsmkcoding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        ambildata()
+    }
+
+    private fun ambildata() {
+        val bundle = intent.extras
+
+        val nama = bundle?.getString("nama1")
+        val gender = bundle?.getString("gender")
+        val umur = bundle?.getString("umur1")
+        val email = bundle?.getString("email")
+        val telp = bundle?.getString("telp1")
+        val alamat = bundle?.getString("alamat1")
+
+        edtname.text = nama
+        jk.text = gender
+        edtumur.text = umur
+        edtemail.text = email
+        edttelp.text = telp
+        edtalamat.text = alamat
     }
 }
