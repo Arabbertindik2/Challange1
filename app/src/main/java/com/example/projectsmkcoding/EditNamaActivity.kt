5dc847f8befb4137b83d18ebf9bf92144624f951
Arabbertindik2 <58155980+arabbertindik2@users.noreply.github.com>
2020-05-06 19:41:14 +0700

    Insyaallah selesai

--- app/src/main/java/com/example/projectsmkcoding/EditNamaActivity.kt
+++ app/src/main/java/com/example/projectsmkcoding/EditNamaActivity.kt
@@ -1,12 +1,34 @@
 package com.example.projectsmkcoding
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nama.*
 
 class EditNamaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nama)
+
+        val intentdata = intent.extras
+        val namaUser = intentdata.getString("nama")
+
+        edtProfilName.setText(namaUser)
+
+        btnEditSave.setOnClickListener { savedata() }
+    }
+
+    private fun savedata() {
+        val namaEdit = edtProfilName.text.toString()
+        if (!namaEdit.isEmpty()) {
+            val result = Intent()
+            result.putExtra("nama", namaEdit)
+            setResult(Activity.RESULT_OK, result)
+        } else {
+            setResult(Activity.RESULT_CANCELED)
+        }
+        finish()
     }
 }
--- app/src/main/java/com/example/projectsmkcoding/ProfileActivity.kt
+++ app/src/main/java/com/example/projectsmkcoding/ProfileActivity.kt
@@ -1,7 +1,12 @@
 package com.example.projectsmkcoding
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_edit_nama.*
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
@@ -11,6 +16,47 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         ambildata()
+        btneditname.setOnClickListener { navigasiedit() }
+        btndial.setOnClickListener { dialPhoneNumber(edttelp.text.toString()) }
+        btnabout.setOnClickListener { about() }
+    }
+
+    private fun about() {
+        val intent = Intent(this, AboutActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun dialPhoneNumber(phoneNumber : String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel: $phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) != null){
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        val REQUEST_CODE = 100
+    }
+
+    private fun navigasiedit() {
+        val intent = Intent(this, EditNamaActivity::class.java)
+
+        val namaUser = edtname.text.toString()
+        intent.putExtra("nama", REQUEST_CODE)
+
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val result = data?.getStringExtra("nama")
+                edtname.text = result
+            } else {
+                Toast.makeText(this, "Edit failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun ambildata() {
